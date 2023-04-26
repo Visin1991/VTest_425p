ad85d33bf6c51c22580373562c7ceea824c1fe00
Visin1991 <zhuzhanhao1991@gmail.com>
2023-04-26 16:42:19 +0800

    finish custom lens flare code

--- Plugins/CustomPostProcess/Source/CustomPostProcess/Private/PostProcessSubsystem.cpp
+++ Plugins/CustomPostProcess/Source/CustomPostProcess/Private/PostProcessSubsystem.cpp
@@ -184,9 +184,88 @@ public:
 };
 IMPLEMENT_GLOBAL_SHADER(FLensFlareHaloPS, "/CustomShaders/Halo.usf", "HaloPS", SF_Pixel);
 
-    // TODO_SHADER_GLARE
+// TODO_SHADER_GLARE
+// Glare shader pass
+class FLensFlareGlareVS : public FGlobalShader
+{
+public:
+    DECLARE_GLOBAL_SHADER(FLensFlareGlareVS);
+    SHADER_USE_PARAMETER_STRUCT(FLensFlareGlareVS, FGlobalShader);
 
-    // TODO_SHADER_MIX
+    BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+        SHADER_PARAMETER_STRUCT_INCLUDE(FCustomLensFlarePassParameters, Pass)
+        SHADER_PARAMETER_SAMPLER(SamplerState, InputSampler)
+        SHADER_PARAMETER(FIntPoint, TileCount)
+        SHADER_PARAMETER(FVector4, PixelSize)
+        SHADER_PARAMETER(FVector2D, BufferSize)
+        END_SHADER_PARAMETER_STRUCT()
+};
+class FLensFlareGlareGS : public FGlobalShader
+{
+public:
+    DECLARE_GLOBAL_SHADER(FLensFlareGlareGS);
+    SHADER_USE_PARAMETER_STRUCT(FLensFlareGlareGS, FGlobalShader);
+
+    BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+        SHADER_PARAMETER(FVector4, PixelSize)
+        SHADER_PARAMETER(FVector2D, BufferSize)
+        SHADER_PARAMETER(FVector2D, BufferRatio)
+        SHADER_PARAMETER(float, GlareIntensity)
+        SHADER_PARAMETER(float, GlareDivider)
+        SHADER_PARAMETER(FVector4, GlareTint)
+        SHADER_PARAMETER_ARRAY(float, GlareScales, [3])
+        END_SHADER_PARAMETER_STRUCT()
+};
+class FLensFlareGlarePS : public FGlobalShader
+{
+public:
+    DECLARE_GLOBAL_SHADER(FLensFlareGlarePS);
+    SHADER_USE_PARAMETER_STRUCT(FLensFlareGlarePS, FGlobalShader);
+
+    BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+        SHADER_PARAMETER_SAMPLER(SamplerState, GlareSampler)
+        SHADER_PARAMETER_TEXTURE(Texture2D, GlareTexture)
+        END_SHADER_PARAMETER_STRUCT()
+
+    static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
+    {
+        return IsFeatureLevelSupported(Parameters.Platform, ERHIFeatureLevel::SM5);
+    }
+};
+IMPLEMENT_GLOBAL_SHADER(FLensFlareGlareVS, "/CustomShaders/Glare.usf", "GlareVS", SF_Vertex);
+IMPLEMENT_GLOBAL_SHADER(FLensFlareGlareGS, "/CustomShaders/Glare.usf", "GlareGS", SF_Geometry);
+IMPLEMENT_GLOBAL_SHADER(FLensFlareGlarePS, "/CustomShaders/Glare.usf", "GlarePS", SF_Pixel);
+
+// Final bloom mix shader
+class FLensFlareBloomMixPS : public FGlobalShader
+{
+public:
+    DECLARE_GLOBAL_SHADER(FLensFlareBloomMixPS);
+    SHADER_USE_PARAMETER_STRUCT(FLensFlareBloomMixPS, FGlobalShader);
+
+    BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+        SHADER_PARAMETER_STRUCT_INCLUDE(FCustomLensFlarePassParameters, Pass)
+        SHADER_PARAMETER_SAMPLER(SamplerState, InputSampler)
+        SHADER_PARAMETER_RDG_TEXTURE(Texture2D, BloomTexture)
+        SHADER_PARAMETER_RDG_TEXTURE(Texture2D, GlareTexture)
+        SHADER_PARAMETER_TEXTURE(Texture2D, GradientTexture)
+        SHADER_PARAMETER_SAMPLER(SamplerState, GradientSampler)
+        SHADER_PARAMETER(FVector4, Tint)
+        SHADER_PARAMETER(FVector2D, InputViewportSize)
+        SHADER_PARAMETER(FVector2D, BufferSize)
+        SHADER_PARAMETER(FVector2D, PixelSize)
+        SHADER_PARAMETER(FIntVector, MixPass)
+        SHADER_PARAMETER(float, Intensity)
+        END_SHADER_PARAMETER_STRUCT()
+
+        static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
+    {
+        return IsFeatureLevelSupported(Parameters.Platform, ERHIFeatureLevel::SM5);
+    }
+};
+IMPLEMENT_GLOBAL_SHADER(FLensFlareBloomMixPS, "/CustomShaders/Mix.usf", "MixPS", SF_Pixel);
+
+
 }
 
 TAutoConsoleVariable<int32> CVarLensFlareRenderBloom(
@@ -441,8 +520,97 @@ void UPostProcessSubsystem::RenderLensFlare(
     }
 
 
-    // TODO_MIX
+    //TODO_MIX
+    {
+        const FString PassName("LensFlareMix");
 
+        FIntRect MixViewport = FIntRect(
+            0,
+            0,
+            View.ViewRect.Width() / 2,
+            View.ViewRect.Height() / 2
+        );
+
+        FVector2D BufferSize = FVector2D(MixViewport.Width(), MixViewport.Height());
+
+        // Create buffer
+        FRDGTextureDesc Description = Inputs.Bloom.Texture->Desc;
+        Description.Reset();
+        Description.Extent = MixViewport.Size();
+        Description.Format = PF_FloatRGBA;
+        Description.ClearValue = FClearValueBinding(FLinearColor::Transparent);
+        FRDGTextureRef MixTexture = GraphBuilder.CreateTexture(Description, *PassName);
+
+        // Shader parameters
+        TShaderMapRef<FCustomScreenPassVS> VertexShader(View.ShaderMap);
+        TShaderMapRef<FLensFlareBloomMixPS> PixelShader(View.ShaderMap);
+
+        FLensFlareBloomMixPS::FParameters* PassParameters = GraphBuilder.AllocParameters<FLensFlareBloomMixPS::FParameters>();
+        PassParameters->Pass.RenderTargets[0] = FRenderTargetBinding(MixTexture, ERenderTargetLoadAction::ENoAction);
+        PassParameters->InputSampler = BilinearClampSampler;
+        PassParameters->GradientTexture = GWhiteTexture->TextureRHI;
+        PassParameters->GradientSampler = BilinearClampSampler;
+        PassParameters->BufferSize = BufferSize;
+        PassParameters->PixelSize = FVector2D(1.0f, 1.0f) / BufferSize;
+        PassParameters->InputViewportSize = BloomInputViewportSize;
+        PassParameters->Tint = FVector4(PostProcessAsset->Tint);
+        PassParameters->Intensity = PostProcessAsset->Intensity;
+
+        if (PostProcessAsset->Gradient != nullptr)
+        {
+            const FTextureRHIRef TextureRHI = PostProcessAsset->Gradient->Resource->TextureRHI;
+            PassParameters->GradientTexture = TextureRHI;
+        }
+        // Plug in buffers
+        const int32 MixBloomPass = CVarLensFlareRenderBloom.GetValueOnRenderThread();
+
+        PassParameters->MixPass = FIntVector(
+            (Inputs.bCompositeWithBloom && MixBloomPass),
+            (FlareTexture != nullptr),
+            (GlareTexture != nullptr)
+        );
+
+        if (Inputs.bCompositeWithBloom && MixBloomPass)
+        {
+            PassParameters->BloomTexture = Inputs.Bloom.Texture;
+        }
+        else
+        {
+            PassParameters->BloomTexture = InputTexture;
+        }
+
+        if (FlareTexture != nullptr)
+        {
+            PassParameters->Pass.InputTexture = FlareTexture;
+        }
+        else
+        {
+            PassParameters->Pass.InputTexture = InputTexture;
+        }
+
+        if (GlareTexture != nullptr)
+        {
+            PassParameters->GlareTexture = GlareTexture;
+        }
+        else
+        {
+            PassParameters->GlareTexture = InputTexture;
+        }
+
+        // Render
+        DrawShaderPass(
+            GraphBuilder,
+            PassName,
+            PassParameters,
+            VertexShader,
+            PixelShader,
+            ClearBlendState,
+            MixViewport
+        );
+
+        OutputTexture = MixTexture;
+        OutputRect = MixViewport;
+    }
 
     ////////////////////////////////////////////////////////////////////////
     // Final Output
@@ -796,6 +964,140 @@ FRDGTextureRef UPostProcessSubsystem::RenderGlare(
     const FViewInfo& View
 )
 {
-    // TODO_GLARE
+    //RDG_EVENT_SCOPE(GraphBuilder, "GlarePass");
+
+    //FRDGTextureRef OutputTexture = nullptr;
+
+    //FIntRect Viewport4 = FIntRect(
+    //    0,
+    //    0,
+    //    View.ViewRect.Width() / 4,
+    //    View.ViewRect.Height() / 4
+    //);
+
+    //// Only render the Glare if its intensity is different from 0
+    //if (PostProcessAsset->GlareIntensity > SMALL_NUMBER)
+    //{
+    //    const FString PassName("LensFlareGlare");
+
+    //    // This compute the number of point that will be drawn
+    //    // Since we want one point for 2 by 2 pixel block we just 
+    //    // need to divide the resolution by two to get this value.
+    //    FIntPoint TileCount = Viewport4.Size();
+    //    TileCount.X = TileCount.X / 2;
+    //    TileCount.Y = TileCount.Y / 2;
+    //    int32 Amount = TileCount.X * TileCount.Y;
+
+    //    // Compute the ratio between the width and height
+    //    // to know how to adjust the scaling of the quads.
+    //    // (This assume width is bigger than height.)
+    //    FVector2D BufferRatio = FVector2D(
+    //        float(Viewport4.Height()) / float(Viewport4.Width()),
+    //        1.0f
+    //    );
+
+    //    // Build the buffer
+    //    FRDGTextureDesc Description = InputTexture->Desc;
+    //    Description.Reset();
+    //    Description.Extent = Viewport4.Size();
+    //    Description.Format = PF_FloatRGB;
+    //    Description.ClearValue = FClearValueBinding(FLinearColor::Transparent);
+    //    FRDGTextureRef GlareTexture = GraphBuilder.CreateTexture(Description, *PassName);
+
+    //    // Setup a few other variables that will 
+    //    // be needed by the shaders.
+    //    FVector4 PixelSize = FVector4(0, 0, 0, 0);
+    //    PixelSize.X = 1.0f / float(Viewport4.Width());
+    //    PixelSize.Y = 1.0f / float(Viewport4.Height());
+    //    PixelSize.Z = PixelSize.X;
+    //    PixelSize.W = PixelSize.Y * -1.0f;
+
+    //    FVector2D BufferSize = FVector2D(Description.Extent);
+
+    //    // Setup shader
+    //    FCustomLensFlarePassParameters* PassParameters = GraphBuilder.AllocParameters<FCustomLensFlarePassParameters>();
+    //    PassParameters->InputTexture = InputTexture;
+    //    PassParameters->RenderTargets[0] = FRenderTargetBinding(GlareTexture, ERenderTargetLoadAction::EClear);
+
+    //    // Vertex shader
+    //    FLensFlareGlareVS::FParameters VertexParameters;
+    //    VertexParameters.Pass = *PassParameters;
+    //    VertexParameters.InputSampler = BilinearBorderSampler;
+    //    VertexParameters.TileCount = TileCount;
+    //    VertexParameters.PixelSize = PixelSize;
+    //    VertexParameters.BufferSize = BufferSize;
+
+    //    // Geometry shader
+    //    FLensFlareGlareGS::FParameters GeometryParameters;
+    //    GeometryParameters.BufferSize = BufferSize;
+    //    GeometryParameters.BufferRatio = BufferRatio;
+    //    GeometryParameters.PixelSize = PixelSize;
+    //    GeometryParameters.GlareIntensity = PostProcessAsset->GlareIntensity;
+    //    GeometryParameters.GlareTint = FVector4(PostProcessAsset->GlareTint);
+    //    GeometryParameters.GlareScales[0] = PostProcessAsset->GlareScale.X;
+    //    GeometryParameters.GlareScales[1] = PostProcessAsset->GlareScale.Y;
+    //    GeometryParameters.GlareScales[2] = PostProcessAsset->GlareScale.Z;
+    //    GeometryParameters.GlareDivider = FMath::Max(PostProcessAsset->GlareDivider, 0.01f);
+
+    //    // Pixel shader
+    //    FLensFlareGlarePS::FParameters PixelParameters;
+    //    PixelParameters.GlareSampler = BilinearClampSampler;
+    //    PixelParameters.GlareTexture = GWhiteTexture->TextureRHI;
+
+    //    if (PostProcessAsset->GlareLineMask != nullptr)
+    //    {
+    //        const FTextureRHIRef TextureRHI = PostProcessAsset->GlareLineMask->Resource->TextureRHI;
+    //        PixelParameters.GlareTexture = TextureRHI;
+    //    }
+
+    //    TShaderMapRef<FLensFlareGlareVS> VertexShader(View.ShaderMap);
+    //    TShaderMapRef<FLensFlareGlareGS> GeometryShader(View.ShaderMap);
+    //    TShaderMapRef<FLensFlareGlarePS> PixelShader(View.ShaderMap);
+
+    //    // Required for Lambda capture
+    //    FRHIBlendState* BlendState = this->AdditiveBlendState;
+
+    //    GraphBuilder.AddPass(
+    //        RDG_EVENT_NAME("%s", *PassName),
+    //        PassParameters,
+    //        ERDGPassFlags::Raster,
+    //        [
+    //            VertexShader, VertexParameters,
+    //            GeometryShader, GeometryParameters,
+    //            PixelShader, PixelParameters,
+    //            BlendState, Viewport4, Amount
+    //        ] (FRHICommandListImmediate& RHICmdList)
+    //        {
+    //            RHICmdList.SetViewport(
+    //                Viewport4.Min.X, Viewport4.Min.Y, 0.0f,
+    //                Viewport4.Max.X, Viewport4.Max.Y, 1.0f
+    //            );
+
+    //            FGraphicsPipelineStateInitializer GraphicsPSOInit;
+    //            RHICmdList.ApplyCachedRenderTargets(GraphicsPSOInit);
+    //            GraphicsPSOInit.BlendState = BlendState;
+    //            GraphicsPSOInit.RasterizerState = TStaticRasterizerState<>::GetRHI();
+    //            GraphicsPSOInit.DepthStencilState = TStaticDepthStencilState<false, CF_Always>::GetRHI();
+    //            GraphicsPSOInit.BoundShaderState.VertexDeclarationRHI = GEmptyVertexDeclaration.VertexDeclarationRHI;
+    //            GraphicsPSOInit.BoundShaderState.VertexShaderRHI = VertexShader.GetVertexShader();
+    //            GraphicsPSOInit.BoundShaderState.GeometryShaderRHI = GeometryShader.GetGeometryShader();
+    //            GraphicsPSOInit.BoundShaderState.PixelShaderRHI = PixelShader.GetPixelShader();
+    //            GraphicsPSOInit.PrimitiveType = PT_PointList;
+    //            SetGraphicsPipelineState(RHICmdList, GraphicsPSOInit);
+
+    //            SetShaderParameters(RHICmdList, VertexShader, VertexShader.GetVertexShader(), VertexParameters);
+    //            SetShaderParameters(RHICmdList, GeometryShader, GeometryShader.GetGeometryShader(), GeometryParameters);
+    //            SetShaderParameters(RHICmdList, PixelShader, PixelShader.GetPixelShader(), PixelParameters);
+
+    //            RHICmdList.SetStreamSource(0, nullptr, 0);
+    //            RHICmdList.DrawPrimitive(0, 1, Amount);
+    //        });
+
+    //    OutputTexture = GlareTexture;
+
+    //} // End of if()
+
+    //return OutputTexture;
+
     return nullptr;
 }
